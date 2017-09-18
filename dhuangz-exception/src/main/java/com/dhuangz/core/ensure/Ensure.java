0cab16187d0a2d92e14755e315135074f71cc2ba
zhangyufei <303427405@qq.com>
2017-09-18 23:44:21 +0800

    东皇钟异常处理

---
+++ dhuangz-exception/src/main/java/com/dhuangz/core/ensure/Ensure.java
@@ -0,0 +1,7 @@
+package com.dhuangz.core.ensure;
+
+/**
+ *  异常处理
+ */
+public class Ensure {
+}
---
+++ dhuangz-exception/src/main/java/com/dhuangz/core/exceptions/DHZExceptionFactory.java
@@ -0,0 +1,8 @@
+package com.dhuangz.core.exceptions;
+
+/**
+ * 东皇钟异常工厂类
+ * Created By zhangyufei on 2017/9/18
+ */
+public class DHZExceptionFactory {
+}
